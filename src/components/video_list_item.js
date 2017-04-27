6e93b92f9bc6a10e3cfdcd141fd08f835bb43108
Emir Okan Işıldar <okanisildar@gmail.com>
2017-04-27 21:57:19 +0300

    First commit

---
+++ src/components/video_list_item.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) => { //It grab the video property from props. Same with video = props.video
+	//const video = props.video;
+	const imageURL = video.snippet.thumbnails.default.url;
+
+	return (
+		<li onClick={() => onVideoSelect(video)} className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageURL}/>
+				</div>
+
+				<div className="media-body">
+					<div className="media-heading">{video.snippet.title}</div>
+				</div>
+			</div>
+		</li>
+	);
+};
+
+export default VideoListItem;
