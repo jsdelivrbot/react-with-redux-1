a09ad2c240465a0131ef182844a2a953af77d037
Emir Okan Işıldar <okanisildar@gmail.com>
2017-04-27 22:57:09 +0300

    reducer_books created

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+//We can import this function and it will return peace of states
+export default function () {
+	return [
+	{title: 'JavaScript'},
+	{title: 'Game Of Thrones'},
+	{title: 'LOTR'},
+	{title: 'Star Wars'}
+	]
+}
+
