7f77fe8e3643d84ea142af9eaac1a5851b13baed
Emir Okan Işıldar <okanisildar@gmail.com>
2017-04-28 23:10:12 +0300

    Some small updates

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,8 @@
-//We can import this function and it will return peace of states
 export default function () {
 	return [
 	{title: 'JavaScript'},
 	{title: 'Game Of Thrones'},
-	{title: 'LOTR'},
+	{title: 'Lord of The Rings'},
 	{title: 'Star Wars'}
 	]
 }
